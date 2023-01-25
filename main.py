3913933e0cd4068f1dd519cb22b3bc8f99eae87e
Charles Tang <ctang5@wpi.edu>
2023-01-25 08:43:56 -0500

    clean up

--- main.py
+++ main.py
@@ -7,65 +7,66 @@ import mysql.connector
 
 
 def on_connect(target):
-		serial = target.sdd_res.hex()
-		tag = nfc.tag.activate(clf, target)
-		print(binascii.hexlify(tag.identifier).decode())
+    serial = target.sdd_res.hex()
+    tag = nfc.tag.activate(clf, target)
+    print(binascii.hexlify(tag.identifier).decode())
+
 
 def on_startup(targets):
-	return targets
-	
+    return targets
+
+
 rdwr_options = {
-	'targets': ['106A'],
-	'on_startup': on_startup,
-	'on_connect': lambda tag: False,
-	} 
+    'targets': ['106A'],
+    'on_startup': on_startup,
+    'on_connect': lambda tag: False,
+}
 
 with nfc.ContactlessFrontend('tty:S0:pn532') as clf:
-	connected = False
-	
-	while not connected:
-		try:
-			mydb = mysql.connector.connect(
-				host="mysql.wpi.edu",
-				user="ctang5",
-				password="CT@ng5",
-				database="mams_siso",
-				)
-			sql = mydb.cursor(buffered=True)
-			print("connected")
-			connected = True
-		except:
-			print("Not connected. Trying again.")
-			sleep(10)
-			continue
+    connected = False
 
-	while True:
-		try:
-			target = clf.sense(RemoteTarget('106A'))
-		except:
-			print('Error in connecting to RFID module.')
-			continue
-		if target is None:
-			print("Scanning")
-			sleep(0.25)
-			continue
-		tag = nfc.tag.activate(clf, target)
-		try:
-			print("———————————————————————————————")
-			command = f"INSERT INTO daily VALUES(get_id({str(binascii.hexlify(tag.identifier).decode())}), CURRENT_DATE(), CURRENT_TIME());"
-			print("RFID ID: " + binascii.hexlify(tag.identifier).decode())
-			sql.execute(command)
-			mydb.commit()
-			command = f"SELECT first_name, last_name FROM person WHERE student_id = get_id({binascii.hexlify(tag.identifier).decode()})"
-			sql.execute(command)
-			result = sql.fetchall()
-			print(f"Welcome {result[0][0]} {result[0][1]}")
-			print("———————————————————————————————")
-			sleep(5)
-		except:
-			print("RFID ID not recognized, please try something else")
-			sleep(2)
-	mydb.close()
+    while not connected:
+        try:
+            mydb = mysql.connector.connect(
+                host="mysql.wpi.edu",
+                user="ctang5",
+                password="CT@ng5",
+                database="mams_siso",
+            )
+            sql = mydb.cursor(buffered=True)
+            print("Connected to MySQL Database.")
+            connected = True
+        except:
+            print("Not connected to WPI MySQL database. Trying again.")
+            sleep(10)
+            continue
 
-	
-	
+    print("Scanning")
+    while True:
+        try:
+            target = clf.sense(RemoteTarget('106A'))
+        except:
+            print('Error in connecting to RFID module.')
+            continue
+        if target is None:
+            sleep(0.25)
+            continue
+        tag = nfc.tag.activate(clf, target)
+        try:
+            print("———————————————————————————————")
+            command = f"INSERT INTO daily VALUES(get_id({str(binascii.hexlify(tag.identifier).decode())}), CURRENT_DATE(), CURRENT_TIME());"
+            print("RFID ID: " + binascii.hexlify(tag.identifier).decode())
+            sql.execute(command)
+            mydb.commit()
+            command = f"SELECT first_name, last_name FROM person WHERE student_id = get_id({binascii.hexlify(tag.identifier).decode()})"
+            sql.execute(command)
+            result = sql.fetchall()
+            print(f"Welcome {result[0][0]} {result[0][1]}!")
+            print("———————————————————————————————")
+            sleep(2)
+            print("Scanning")
+        except:
+            print("RFID ID not recognized, please try something else")
+            sleep(2)
+            print("Scanning")
+    mydb.close()
---
+++ utils/uploadrfid.py
@@ -0,0 +1,52 @@
+import csv
+import mysql.connector
+from time import sleep
+
+with open("utils/rfid.csv", newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    counter = 0
+
+    #Connect to WPI MySQL
+    connected = False 
+    while not connected:
+        try:
+            mydb = mysql.connector.connect(
+                host="mysql.wpi.edu",
+                user="ctang5",
+                password="CT@ng5",
+                database="mams_siso",
+            )
+            sql = mydb.cursor(buffered=True)
+            print("Connected to MySQL Database.")
+            connected = True
+        except:
+            print("Not connected to WPI MySQL database. Trying again.")
+            sleep(10)
+            continue
+    
+    #Reset RFID Table in MySQL
+    command = "DELETE FROM rfid;"
+    sql.execute(command)
+    mydb.commit()
+    print("Reset RFID Table")
+
+    #Read through rfid.csv
+    for line in csvreader:
+        if counter == 0:
+            counter+=1
+            continue
+        id = int(line[0])
+        rfid_uid = line[1]
+        if id == "" or rfid_uid == "":
+            continue
+        fname = line[3]
+        lname = line[2]
+        command = f"INSERT INTO rfid VALUES({id}, {rfid_uid});"
+        sql.execute(command)
+        mydb.commit()
+    
+    print("All RFID entries updated")
+
+        
+    
+        
